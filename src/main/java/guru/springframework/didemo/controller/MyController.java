41ea676916b55fc0e56a102e381924e6b1a8c736
Arnab Kundu <arnabkundu@hotmail.com>
2019-05-04 20:51:44 +0100

    Committing DI

---
+++ src/main/java/guru/springframework/didemo/controller/MyController.java
@@ -0,0 +1,13 @@
+package guru.springframework.didemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public void hello(){
+        System.out.println("Hello !!!");
+    }
+}
+
+
+
